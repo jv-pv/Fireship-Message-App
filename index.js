53a76388f6df4e61415fe7b8530cdb5b7763a038
John Pieri <jvpieri@gmail.com>
2023-06-26 15:15:57 -0400

    Added from and to functionality

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const database = getDatabase(app)
 const endorsementsDB = ref(database, "Endorsements")
 
 const textArea = document.getElementById('input-field')
+const inputFrom = document.querySelector('.input-from')
+const inputTo = document.querySelector('.input-to')
 const publishBtn = document.getElementById('publish-button')
 const endorsementUl = document.getElementById('endorsements-list')
 
@@ -19,38 +21,66 @@ textArea.addEventListener('keyup', (e) => {
 
 publishBtn.addEventListener('click', () => {
     let textAreaInputValue = textArea.value
-    clearField()
-    appendListItemToHtml(textAreaInputValue)
-    push(endorsementsDB, textAreaInputValue)
+    let inputFromValue = inputFrom.value
+    let inputToValue = inputTo.value
+
+    let endorsementObj = {
+        endorsementText: textAreaInputValue,
+        endorsementFrom: inputFromValue,
+        endorsementTo: inputToValue,
+        endorsementLikes: ``
+    }
+
+    push(endorsementsDB, endorsementObj)
+    clearInputFields()
 })
 
 onValue(endorsementsDB, (snapshot) => {
-    let endorsementsList = Object.entries(snapshot.val())
-    console.log(endorsementsList)
-    clearField()
-
-    endorsementsList.forEach(endorsement => {
-        appendListItemToHtml(endorsement)
-    })
-
+    if (snapshot.exists()) {
+        let endorsementsList = Object.entries(snapshot.val())
+        clearList()
+        endorsementsList.forEach(endorsement => {
+            appendListItemToHtml(endorsement)
+        })
+    } else {
+        endorsementUl.innerHTML = "<p>Empty</p>"
+    }
 })
 
 function appendListItemToHtml(endorsements) {
     let listEl = document.createElement('li')
 
     let endorsementId = endorsements[0]
-    let endorsementValue = endorsements[1]
+    let endorsementObj = endorsements[1]
+
+    console.log(endorsementId)
 
     listEl.addEventListener('dblclick', () => {
         let exactLocationOfEndorsements = ref(database, `Endorsements/${endorsementId}`)
         remove(exactLocationOfEndorsements)
     })
+    const {endorsementText, endorsementFrom, endorsementTo, endorsementLikes} = endorsementObj
 
-    listEl.textContent = endorsementValue
+    listEl.innerHTML = `
+        <div class="endorsement-details">
+            <h3 class="endorsement-to">${endorsementTo}</h3>
+            <p class="endorsement-text">${endorsementText}</p>
+            <h3 class="endorsement-from">${endorsementFrom}</h3>
+            <span class="endorsement-likes">
+                <i class="fa-solid fa-heart"></i>
+                <p>${endorsementLikes}</p>
+            </span>
+        </div>`
+        
     endorsementUl.append(listEl)
 }
 
-function clearField() {
+function clearInputFields() {
     textArea.value = ""
+    inputFrom.value = ""
+    inputTo.value = ""
+}
+
+function clearList() {
     endorsementUl.innerHTML = ""
 }
